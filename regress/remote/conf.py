a63bb8c9acf14d0e44a5a096327352e3fc98f032
Jay Lang <jaytlang9@gmail.com>
2023-08-26 14:12:50 -0400

    delete remote to make way for submodule

--- regress/remote/conf.py
+++
@@ -1,8 +0,0 @@
-# Configure me!
-
-client_cert = "../etc/jaytlang.pem"
-client_key = "../etc/jaytlang.key"
-client_ca = "../etc/mitcca.pem"
-
-server_hostname = "eecs-digital-51.mit.edu"
-server_port = 443
